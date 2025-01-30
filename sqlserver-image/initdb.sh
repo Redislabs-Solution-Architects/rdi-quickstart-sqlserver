531ccc7f7f67c371c45383064880326904b587da
loriotpiroloriol <george.vonbulow@redis.com>
2025-01-30 10:46:18 +0000

    Changed method for checking availability of SQL Server

--- sqlserver-image/initdb.sh
+++ sqlserver-image/initdb.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-if ! [ -f /var/opt/mssql/data/Chinook.mdf ]; then
-  while ! </dev/tcp/localhost/1433 2>/dev/null; do
-    sleep 2
+if [ ! -f /var/opt/mssql/data/Chinook.mdf ]; then
+  #Wait for SQL Server to be available
+  while true; do
+    if [ ! $(/opt/mssql-tools18/bin/sqlcmd -No -h -1 -t 1 -S localhost -U ${MSSQL_USER} -P "${MSSQL_SA_PASSWORD}" -Q "SET NOCOUNT ON; Select SUM(state) from sys.databases") ]; then
+      sleep 2
+    else
+      break
+    fi
   done
   echo "Creating Chinook database..."
   sed -e "s/<DBZUSER>/${DBZUSER}/g" \
